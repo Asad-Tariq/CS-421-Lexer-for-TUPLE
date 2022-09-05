ec8db8470f8948537b51de03650857c1b33b900b
Asad-Tariq <at05439@st.habib.edu.pk>
2022-09-05 23:13:48 +0500

    Basic Lexer class and code (methods)
    Added the ability to detect arithmetic and relational operators, newline, blanks/spaces, string literals and comments

---
+++ lexer.py
@@ -0,0 +1,79 @@
+keywords = ["and", "break", "continue", "else", "false", "for", "if", "mod", "not", "or", "then", "true", "void", "while"]
+datatypes = ["bool", "char", "int", "float"]
+punctuators = ["{", "}", "(", ")", ";", "[", "]", "\'", "\"", ",", "."]
+relational_ops_single = {"<":"LT", ">":"GT"}
+realtional_op_double = {"<=":"LE", ">=":"GE", "==":"EQ", "!=":"NE"}
+arithmetic_op = ["+", "-", "*", "/", "^"]
+whitespaces = {" ":"blank", "\n":"newline", "\t":"tab"}
+
+class Lexer:
+    def __init__(self, input) -> None:
+        self.input = input + '\n'
+        self.curChar = ''
+        self.curPos = -1
+        self.nextChar()
+
+    # Process the next character
+    def nextChar(self):
+        self.curPos += 1
+        if self.curPos >= len(self.input):
+            self.curChar = '\0' # EOF char
+        else:
+            self.curChar = self.input[self.curPos]
+
+    # Return the lookahead character
+    def peek(self):
+        if self.curPos + 1 >= len(self.input):
+            return '\0'
+        return self.input[self.curPos + 1]
+    
+    # Detect the whitespaces in our code
+    def detectWhitespace(self):
+        pass
+
+    # Detect the comments in our code
+    def detectComments(self):
+        pass
+
+    # Return the next token
+    def getToken(self):
+        token = []
+        if self.curChar == "/":
+            if self.peek() == "$":
+                self.nextChar()
+                self.nextChar()
+                while self.curChar != "$":
+                    self.nextChar()
+                if self.peek() == "/":
+                    token.append("<comment>")
+                    self.nextChar()
+                else:
+                    token.append("<comment not closed properly!>")
+            elif self.curChar in arithmetic_op:
+                token.append("<" + self.curChar + ">")
+        elif self.curChar in arithmetic_op:
+            token.append("<" + self.curChar + ">")
+        elif self.curChar in relational_ops_single:
+            if self.peek() == "=":
+                key = self.curChar + "="
+                token.append("<rel_op, " + realtional_op_double[key] + ">")
+                self.nextChar()
+            else:
+                key = self.curChar
+                token.append("<rel_op, " + relational_ops_single[key] + ">")
+        elif self.curChar == "\"":
+            save_string = ""
+            self.nextChar()
+            while self.curChar != "\"":
+                save_string += self.curChar
+                self.nextChar()
+            token.append("<literal, " + save_string + ">")
+        elif self.curChar in punctuators:
+            token.append("<punctuator, " + self.curChar + ">")    
+        elif self.curChar in whitespaces.keys():
+            token.append("<" + whitespaces[self.curChar] + ">")
+        else:
+            pass
+
+        self.nextChar()
+        return token
---
+++ tuple.py
@@ -0,0 +1,11 @@
+from lexer import *
+
+def main():
+    with open("custom_test.tpl") as custom_test:
+        lines = custom_test.readlines()
+
+    for line in lines:
+        lexer = Lexer(line)
+        while lexer.peek() != '\0':
+            print(lexer.getToken())
+main()
