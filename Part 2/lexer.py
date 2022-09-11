8b1990b56784accf60137558391e7d80b1fde743
Fahad Shaikh <fs05452@st.habib.edu.pk>
2022-09-11 14:59:17 +0500

    Chore: document and type annotate the Lexer class.

--- Part 2/lexer.py
+++ Part 2/lexer.py
@@ -1,8 +1,24 @@
 from tuple_spec import *
+from typing import Dict, Tuple
 
 
 class Lexer:
-    def __init__(self, input_stream, symbol_table, symbol_count) -> None:
+    """An lexical analyzer."""
+
+    def __init__(self, input_stream: str, symbol_table: Dict[int, str], symbol_count: int) -> None:
+        """Initializes the lexer for the given portion of the input stream.
+
+        Args:
+        - self: this lexer, the one to create. Mandatory object reference.
+        - input_stream: the portion of the input stream being tokenized - a
+        particular line.
+        - symbol_table: the maintained symbol table.
+        - symbol_count: the number of entries in the symbol table.
+
+        Returns:
+        None.
+        """
+
         self.input = input_stream + '\n'
         self.cur_char = ''
         self.cur_pos = -1
@@ -11,34 +27,61 @@ class Lexer:
         self.error = ""
         self.next_char()
 
-    # Process the next character
-    def next_char(self):
-        self.cur_pos += 1
+    def next_char(self, step: int = 1) -> None:
+        """Updates the file pointer by the given step - a default jump of
+        one - and in turn, updates the current character being processed.
+
+        Args:
+        - self: mandatory object reference.
+        - step: an integer corresponding to the increment to the file pointer.
+        It has a default value of one.
+
+        Returns:
+        None.
+        """
+
+        self.cur_pos += step
         if self.cur_pos >= len(self.input):
             self.cur_char = '\0'  # EOF char
         else:
             self.cur_char = self.input[self.cur_pos]
 
-    # Return the lookahead character
-    def peek(self):
+    def peek(self) -> str:
+        """Returns the lookahead character.
+
+        Args:
+        - self: mandatory object reference.
+
+        Returns:
+        the character at the next position from the one currently being
+        pointed to.
+        """
+
         if self.cur_pos + 1 >= len(self.input):
             return '\0'
         return self.input[self.cur_pos + 1]
 
-    # Detect comment
-    def check_comment(self):
-        tok = ""
+    def check_comment(self) -> str:
+        """Evaluates and returns a token corresponding to whether or not
+        a valid comment is encountered or not.
+
+        Args:
+        - self: mandatory object reference.
+
+        Returns:
+        a token that denotes the validity of the encountered comment.
+        """
+
+        token = ""
         if self.peek() == "$":
-            self.next_char()
-            self.next_char()
+            self.next_char(2)
             while self.cur_char != "$":
                 self.next_char()
             if self.peek() == "/":
-                tok = "<Comment>"
-                self.next_char()
-                self.next_char()
+                token = "<Comment>"
+                self.next_char(2)
             elif self.peek() == "\n":
-                tok = "<Invalid Comment>"
+                token = "<Invalid Comment>"
                 self.error = "Comment not closed properly!"
                 while self.cur_char != "\n":
                     self.next_char()
@@ -46,43 +89,65 @@ class Lexer:
                 while self.peek() != "/":
                     self.next_char()
                 if self.peek() == "/":
-                    tok = "<comment>"
-                    self.next_char()
-                    self.next_char()
+                    token = "<comment>"
+                    self.next_char(2)
         elif self.cur_char in arithmetic_op:
-            tok = "<" + self.cur_char + ">"
+            # if a '/' is encountered, record as arithmetic operator
+            token = f'<{self.cur_char}>'
             self.next_char()
 
-        return tok
+        return token
 
-    # Detect keyword, datatype, identifier
-    def check_key_dt_id(self):
+    def check_key_dt_id(self) -> str:
+        """Evaluates and returns a token corresponding to whether or not
+        a keyword, data-type or (valid/invalid) identifier is encountered.
+
+        Args:
+        - self: mandatory object reference.
+
+        Returns:
+        a token that denotes whether or not a reserved word, data-type or
+        identifier is encountered.
+        """
+
         save_string = ""
-        tok = ""
+        token = ""
         while self.cur_char in letters or self.cur_char in digits or self.cur_char in underscore:
             save_string += self.cur_char
             self.next_char()
         if self.cur_char == ".":
-            tok = "<Invalid identifier!>"
+            token = "<Invalid identifier!>"
             self.error = f'{save_string}{self.cur_char} (Invalid Identifier!)'
             self.next_char()
-        elif self.cur_char not in whitespaces.keys() and self.cur_char not in punctuation\
+        elif self.cur_char not in whitespaces.keys() and self.cur_char not in punctuation \
                 and self.cur_char not in arithmetic_op:
-            tok = "<Invalid Identifier!>"
+            token = "<Invalid Identifier!>"
             self.error = f'{save_string} (Invalid Identifier!)'
         elif save_string in keywords:
-            tok = "<keyword, " + save_string + ">"
+            token = f'<keyword, {save_string}>'
         elif save_string in data_types:
-            tok = "<dt, " + save_string + ">"
+            token = f'<dt, {save_string}>'
         else:
-            tok = "<id, " + save_string + ">"
-            if save_string + ", id" not in self.symbol_table.values():
-                self.symbol_table[self.symbol_count] = save_string + ", id"
+            token = f'<id, {save_string}>'
+            if f'{save_string}, id' not in self.symbol_table.values():
+                self.symbol_table[self.symbol_count] = f'{save_string}, id'
                 self.symbol_count += 1
-        return tok
 
-    # Detect float numbers
-    def checkFloat(self):
+        return token
+
+    def checkFloat(self) -> Tuple[str, bool]:
+        """Evaluates and returns a string and a boolean flag corresponding
+        to the read float value and whether or not the value is valid/invalid,
+        respectively.
+
+        Args:
+        - self: mandatory object reference.
+
+        Returns:
+        a tuple of both the read float value and a boolean flag denoting if the
+        read value is valid or not.
+        """
+
         save_string = ""
         if self.peek() not in digits and self.peek() != "E":
             while self.cur_char != "\n":
@@ -93,7 +158,7 @@ class Lexer:
             save_string += self.cur_char
             self.next_char()
             if self.cur_char in digits:
-                if self.peek() in [punc for punc in punctuation if punc != "."]\
+                if self.peek() in [punc for punc in punctuation if punc != "."] \
                         or self.peek() in whitespaces.keys():
                     save_string += self.cur_char
                     self.next_char()
@@ -109,18 +174,18 @@ class Lexer:
                     else:
                         save_string += self.cur_char
                         self.next_char()
-                        return save_string, True 
+                        return save_string, True
                 elif self.peek() in digits:
                     while self.cur_char in digits:
                         save_string += self.cur_char
                         self.next_char()
-                    if self.cur_char in [punc for punc in punctuation if punc != "."]\
+                    if self.cur_char in [punc for punc in punctuation if punc != "."] \
                             or self.cur_char in whitespaces.keys():
                         return save_string, True
                     if self.cur_char not in digits:
                         if self.cur_char == "E":
                             if self.peek() not in digits:
-                                while self.cur_char not in [punc for punc in punctuation if punc != "."]\
+                                while self.cur_char not in [punc for punc in punctuation if punc != "."] \
                                         and self.cur_char not in whitespaces.keys():
                                     save_string += self.cur_char
                                     self.next_char()
@@ -144,23 +209,31 @@ class Lexer:
                     else:
                         return save_string, True
                 else:
-                    while self.cur_char not in [punc for punc in punctuation if punc != "."]\
+                    while self.cur_char not in [punc for punc in punctuation if punc != "."] \
                             and self.cur_char not in whitespaces.keys():
                         save_string += self.cur_char
                         self.next_char()
-                    return save_string, False 
+                    return save_string, False
             else:
                 while self.cur_char != "\n":
                     save_string += self.cur_char
                     self.next_char()
                 return save_string, False
 
-    # Detect digits
-    def check_digit(self):
+    def check_digit(self) -> str:
+        """Evaluates and returns a token corresponding to the read number.
+
+        Args:
+        - self: mandatory object reference.
+
+        Returns:
+        a string tokenizing the read numeric value.
+        """
+
         save_string = ""
-        tok = ""
+        token = ""
         if self.peek() in letters:
-            tok = "<Unsupported character>"
+            token = "<Unsupported character>"
             self.error = f'{save_string} (Unsupported character found with digit!)'
         else:
             while self.cur_char in digits:
@@ -169,17 +242,26 @@ class Lexer:
             if self.cur_char == ".":
                 floatString, isFloat = self.checkFloat()
                 if isFloat:
-                    tok = f'<float, {save_string}{floatString}>'
+                    token = f'<float, {save_string}{floatString}>'
                 else:
-                    tok = "<Invalid Float!>"
+                    token = "<Invalid Float!>"
                     self.error = f'{save_string}{floatString} (Invalid Float!)'
             else:
-                tok = "<num, " + save_string + ">"
+                token = f'<num, {save_string}>'
 
-        return tok
+        return token
 
-    # Detect arithmetic operator
-    def check_arith_op(self):
+    def check_arith_op(self) -> str:
+        """Evaluates and returns a token corresponding to the read arithmetic
+        operator or a negative numeric value.
+
+        Args:
+        - self: mandatory object reference.
+
+        Returns:
+        a string tokenizing the read arithmetic operator or negative numeric value.
+        """
+
         save_string = ""
         if self.cur_char == "-" and self.peek() in digits:
             save_string += self.cur_char
@@ -187,31 +269,49 @@ class Lexer:
             while self.cur_char in digits:
                 save_string += self.cur_char
                 self.next_char()
-            tok = f'<num, {save_string}>'
+            token = f'<num, {save_string}>'
         else:
-            tok = f'<{self.cur_char}>'
+            token = f'<{self.cur_char}>'
             self.next_char()
-        return tok
 
-    # Detect assignment operator
-    def check_assign_op(self):
+        return token
+
+    def check_assign_op(self) -> str:
+        """Evaluates and returns a token corresponding to the read assignment
+        operator.
+
+        Args:
+        - self: mandatory object reference.
+
+        Returns:
+        a string tokenizing the read assignment operator.
+        """
+
         if self.peek() != "=":
-            tok = f'<assign, {self.cur_char}>'
+            token = f'<assign, {self.cur_char}>'
             self.next_char()
-            return tok
+            return token
         else:
-            tok = self.check_rel_op()
+            token = self.check_rel_op()
             self.next_char()
 
-        return tok
+        return token
 
-    # Detect relational operator
-    def check_rel_op(self):
+    def check_rel_op(self) -> str:
+        """Evaluates and returns a token corresponding to the read relational
+        operator.
+
+        Args:
+        - self: mandatory object reference.
+
+        Returns:
+        a string tokenizing the read relational operator.
+        """
+
         if self.peek() == "=":
             key = self.cur_char + "="
             tok = f'<rel_op, {relational_op_double[key]}>'
-            self.next_char()
-            self.next_char()
+            self.next_char(2)
         else:
             key = self.cur_char
             tok = f'<rel_op, {relational_ops_single[key]}>'
@@ -219,49 +319,95 @@ class Lexer:
 
         return tok
 
-    # Detect string literal
-    def check_string_literal(self):
+    def check_string_literal(self) -> str:
+        """Evaluates and returns a token corresponding to the read string
+        literal.
+
+        Args:
+        - self: mandatory object reference.
+
+        Returns:
+        a string tokenizing the read string literal.
+        """
+
         save_string = ""
         self.next_char()
         while self.cur_char != "\"":
             save_string += self.cur_char
             self.next_char()
-        tok = f'<literal, {save_string}>'
+        token = f'<literal, {save_string}>'
         self.next_char()
 
-        return tok
+        return token
 
-    # Detect character constant
-    def check_char_const(self):
+    def check_char_const(self) -> str:
+        """Evaluates and returns a token corresponding to the read character
+        literal.
+
+        Args:
+        - self: mandatory object reference.
+
+        Returns:
+        a string tokenizing the read character literal.
+        """
+
         save_string = self.cur_char
         self.next_char()
         while self.cur_char != "'" and self.cur_char != "\n" and self.cur_char not in punctuation:
             save_string += self.cur_char
             self.next_char()
         if len(save_string) == 1:
-            tok = f'<char_constant, {save_string}>'
+            token = f'<char_constant, {save_string}>'
         else:
-            tok = f'<Invalid char constant!, {save_string}>'
+            token = f'<Invalid char constant!, {save_string}>'
             self.error = f'{save_string} (Invalid char constant!)'
         if self.peek() != "\0":
             self.next_char()
 
-        return tok
+        return token
 
-    # Detect punctuation
-    def check_punctuation(self):
-        tok = f'<punctuator, {self.cur_char}>'
+    def check_punctuation(self) -> str:
+        """Evaluates and returns a token corresponding to the read punctuator.
+
+        Args:
+        - self: mandatory object reference.
+
+        Returns:
+        a string tokenizing the read punctuator.
+        """
+
+        token = f'<punctuator, {self.cur_char}>'
         self.next_char()
-        return tok
+        return token
 
-    # Detect whitespaces
-    def check_whitespaces(self):
+    def check_whitespaces(self) -> str:
+        """Evaluates and returns a token corresponding to the read whitespace
+        character.
+
+        Args:
+        - self: mandatory object reference.
+
+        Returns:
+        a string tokenizing the read whitespace character.
+        """
+
         tok = f'<{whitespaces[self.cur_char]}>'
         self.next_char()
         return tok
 
-    # Return the next token
-    def get_token(self):
+    def get_token(self) -> Tuple[str, Dict[int, str], int, str]:
+        """On the basis of the character encountered, the most relevant
+        category is determined and in turn, a token is generated.
+
+        Args:
+        - self: mandatory object reference.
+
+        Returns:
+        a tuple containing the generated token, the updated symbol table, symbol
+        count and a error message if the character(s) encountered during tokenization
+        were invalid.
+        """
+
         token = ""
         if self.cur_char == "/":
             token = self.check_comment()
